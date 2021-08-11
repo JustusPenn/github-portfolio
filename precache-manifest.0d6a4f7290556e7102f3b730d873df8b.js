8c56523e446a14cfcb22b16c2747baa55b19c87d
Justus Penn <justuspenn@hotmail.com>
2021-08-11 11:22:19 +0100

    deploy

---
+++ precache-manifest.0d6a4f7290556e7102f3b730d873df8b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1bbb3bbec9a091b60d6c",
+    "url": "/github-portfolio/css/app.c2e3b09f.css"
+  },
+  {
+    "revision": "e265d1bbe331c5d1ef1689d69324977a",
+    "url": "/github-portfolio/index.html"
+  },
+  {
+    "revision": "1bbb3bbec9a091b60d6c",
+    "url": "/github-portfolio/js/app.c8533962.js"
+  },
+  {
+    "revision": "aa97bdfbfabbba0a6eb7",
+    "url": "/github-portfolio/js/chunk-vendors.95422b09.js"
+  },
+  {
+    "revision": "887261c6228c4a9daf91acd9e3937cad",
+    "url": "/github-portfolio/manifest.json"
+  },
+  {
+    "revision": "3ab87cc49e22d7cf8b467fd575457532",
+    "url": "/github-portfolio/octokit-rest.min.js"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/github-portfolio/robots.txt"
+  }
+]);
