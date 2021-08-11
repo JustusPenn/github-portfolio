9af5b00e371d40d7b0cc1bd9c5c8051a34d93c03
Justus Penn <justuspenn@hotmail.com>
2021-08-11 10:52:21 +0100

    Display App

---
+++ dist/precache-manifest.9a3ef288f9d051f869e509a467eed3a6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be973f27e76d76fa3e24",
+    "url": "/css/app.78983b40.css"
+  },
+  {
+    "revision": "9675916dd684968b4fb86671779c9a19",
+    "url": "/index.html"
+  },
+  {
+    "revision": "be973f27e76d76fa3e24",
+    "url": "/js/app.841c9e1e.js"
+  },
+  {
+    "revision": "77eb1d7d39c3e1a02192",
+    "url": "/js/chunk-vendors.1b908136.js"
+  },
+  {
+    "revision": "887261c6228c4a9daf91acd9e3937cad",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "3ab87cc49e22d7cf8b467fd575457532",
+    "url": "/octokit-rest.min.js"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
